ddc32eee6be811b369dc11a7e5a7523356b9748c
OnewayYoun <onewayy91@gmail.com>
2021-11-05 09:31:08 +0900

    update 피로도.py (added backtracking version)

--- Problem Solving/01주차/피로도.py
+++ Problem Solving/01주차/피로도.py
@@ -15,6 +15,29 @@ def solution(k, dungeons):
     return max(possible_cases)
 
 
+def solution_with_backtracking(k, dungeons):
+    global answer
+    answer = 0
+    n = len(dungeons)
+    visited = [False] * n
+
+    def dfs(k, dungeons, cnt):
+        global answer
+        if cnt > answer:
+            answer = cnt
+
+        for i in range(n):
+            if dungeons[i][0] <= k and not visited[i]:
+                visited[i] = True
+                dfs(k - dungeons[i][1], dungeons, cnt+1)
+                visited[i] = False
+
+    dfs(k, dungeons, 0)
+
+    return answer
+
+
 if __name__ == '__main__':
     k, dungeons, result = 80, [[80, 20], [50, 40], [30, 10]], 3
     print(solution(k, dungeons))
+    print(solution_with_backtracking(k, dungeons))
