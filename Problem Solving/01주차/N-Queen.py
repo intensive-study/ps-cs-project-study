e4ec365a9651473fb28b99a3d268babb12b0e586
OnewayYoun <onewayy91@gmail.com>
2021-11-05 12:08:54 +0900

    create & update N-Queen.py (대각선 logic 추가 필요)

---
+++ Problem Solving/01주차/N-Queen.py
@@ -0,0 +1,54 @@
+import sys
+input = sys.stdin.readline
+
+
+def queen_cnt(l):
+    cnt = 0
+    for i in l:
+        cnt += i.count(True)
+    return cnt
+
+
+def make_it_tuple(visited):
+    l = []
+    for i in visited:
+        l.append(tuple(i))
+    return tuple(l)
+
+
+def dfs():
+    global answer
+    if queen_cnt(visited) == N:
+        tmp = make_it_tuple(visited)
+        if tmp not in check:
+            check.add(tmp)
+            answer += 1
+
+    for i in range(N):
+        for j in range(N):
+            if not visited[i][j]:
+                if True not in visited[i] and True not in list(zip(*visited))[j]:       # 가로, 세로 체크
+                    visited[i][j] = True
+                    dfs()
+                    visited[i][j] = False
+
+    return
+
+
+if __name__ == '__main__':
+    global answer, check
+    check = set()
+    answer = 0
+    N = int(input())
+    visited = [[False] * N for _ in range(N)]
+    dfs()
+
+    print(answer)
+
+
+
+"""
+0 0   0 0 0
+0 0   0 0 0
+      0 0 0
+"""
