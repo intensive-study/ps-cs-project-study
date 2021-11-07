ca4c07f801612c2b0470f35c1a3401133d0e887a
OnewayYoun <onewayy91@gmail.com>
2021-11-07 14:17:22 +0900

    create & update selection_sort.py

---
+++ CS/Algorithm/selection_sort.py
@@ -0,0 +1,14 @@
+def selection_sort(l):
+    n = len(l)
+    for i in range(n - 1):
+        min_idx = i
+        for j in range(i + 1, n):
+            if l[min_idx] > l[j]:
+                min_idx = j
+        l[i], l[min_idx] = l[min_idx], l[i]
+
+
+a = [2, 3, 4, 5, 1]
+print(a)
+selection_sort(a)
+print(a)
