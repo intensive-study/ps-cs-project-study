e7a27c1497f6bedbe2ea67b49d001137a380f2d3
OnewayYoun <onewayy91@gmail.com>
2021-11-07 11:41:58 +0900

    MVC와 템플릿 엔진

--- Spring Project/onewayy/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ Spring Project/onewayy/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,4 +14,11 @@ public class HelloController {
         model.addAttribute("data", "hello");
         return "hello"; // resources:templates/hello.html 렌더링
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
 }
