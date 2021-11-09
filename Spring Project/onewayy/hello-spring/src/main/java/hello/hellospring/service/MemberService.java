b5f1625cfd2c09ba174cfcc04cd55e141c5f98c1
OnewayYoun <onewayy91@gmail.com>
2021-11-09 09:44:53 +0900

    회원 서비스 테스트

--- Spring Project/onewayy/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ Spring Project/onewayy/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -10,7 +10,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원가입
@@ -25,7 +29,7 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m-> {
-                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
 
