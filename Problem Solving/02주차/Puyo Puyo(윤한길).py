a93a32853ee10451e19b2db81dd57e239d272c51
OnewayYoun <onewayy91@gmail.com>
2021-11-15 20:14:55 +0900

    update Puyo Puyo(윤한길).py - 구현 & 그래프 탐색

--- Problem Solving/02주차/Puyo Puyo(윤한길).py
+++ Problem Solving/02주차/Puyo Puyo(윤한길).py
@@ -1,9 +1,71 @@
+from collections import deque
+
+
+def bfs(x, y, color):
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+    dq = deque([(x, y)])
+    visited[y][x] = True
+    coordinate = deque([(x, y)])
+
+    while dq:
+        x, y = dq.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < X and 0 <= ny < Y:
+                if graph[ny][nx] == color and not visited[ny][nx]:
+                    dq.append([nx, ny])
+                    coordinate.append((nx, ny))
+                    visited[ny][nx] = True
+
+    if len(coordinate) >= 4:
+        while coordinate:
+            idx_x, idx_y = coordinate.popleft()
+            graph[idx_y][idx_x] = '.'
+        return True
+    else:
+        return False
+
+
+def drop():
+    for i in range(X):
+        idx = Y - 1
+        for j in range(Y - 1, -1, -1):
+            if graph[j][i] == '.':
+                continue
+            else:
+                graph[idx][i], graph[j][i] = graph[j][i], graph[idx][i]
+                idx -= 1
+
+
 if __name__ == '__main__':
-    x, y = 6, 12
-    graph = [input() for _ in range(y)]
-    print(graph)
+    cnt = 0
+    X, Y = 6, 12
+    graph = [list(input()) for _ in range(Y)]
 
+    while True:
+        visited = [[False] * X for _ in range(Y)]
+        flag = False
 
+        for i in range(Y):
+            for j in range(X):
+                if graph[i][j] != '.' and not visited[i][j]:
+                    if bfs(j, i, graph[i][j]):
+                        flag = True
+
+        if flag:
+            cnt += 1
+        else:
+            break
+        # print(*graph, sep='\n')
+        # print()
+        drop()
+
+    # print(*graph, sep='\n')
+    print(cnt)
 
 """
 ......
