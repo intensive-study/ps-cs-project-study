57c07625e4b96cd6ed28da9f110bfa9ad29d53b6
OnewayYoun <onewayy91@gmail.com>
2021-11-23 19:06:37 +0900

    80퍼에서 실패ㅜㅜ

---
+++ Problem Solving/03주차/고양이(윤한길).py
@@ -0,0 +1,40 @@
+from collections import deque
+
+if __name__ == '__main__':
+    N = int(input())
+    str_array = input()
+    cnt = set([str_array[0]])
+    dq = deque()
+    dq.append(str_array[0])
+    answer = 0
+
+    right = 1
+    while dq:
+        while len(cnt) <= N and right < len(str_array):
+            dq.append(str_array[right])
+            cnt.add(str_array[right])
+            right += 1
+
+        if len(str_array) == right:
+            answer = max(len(dq), answer)
+            break
+
+        tmp = dq.popleft()
+        answer = max(len(dq), answer)
+
+        while tmp in dq:
+            tmp = dq.popleft()
+        if tmp in cnt:
+            cnt.remove(tmp)
+
+    print(answer)
+
+"""
+2
+abbcaccba
+result = 4
+
+5
+aaaaa
+result = 5
+"""
