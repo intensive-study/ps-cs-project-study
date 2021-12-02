5ff10c0ed61bbdf0e4616b237d9b92420c661f6d
Giyoon Park <workspaceforict@gmail.com>
2021-12-02 18:53:22 +0900

    Add: 카드 정렬하기(giyoon)

---
+++ Problem Solving/04주차/카드 정렬하기(기윤).py
@@ -0,0 +1,20 @@
+from heapq import heappop, heappush, heapify
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+bundles = [int(input()) for _ in range(n)]
+heapify(bundles)
+answer = 0
+first = True
+
+while bundles:
+    if n == 1:
+        if bundles[0] == 1: break
+        answer = heappop(bundles)
+    else:
+        k = heappop(bundles) + heappop(bundles)
+        answer += k
+        if bundles: heappush(bundles, k)
+
+print(answer)
