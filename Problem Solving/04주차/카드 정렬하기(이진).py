ca6eb3e43ce5353f9a88d402bb2c8d22364ed87e
Yijin <yijin3018@gmail.com>
2021-12-02 15:31:06 +0900

    update 카드정렬하기(이진).py

--- Problem Solving/04주차/카드 정렬하기(이진).py
+++ Problem Solving/04주차/카드 정렬하기(이진).py
@@ -3,6 +3,7 @@ Python3 1008ms
 Pypy3 시간초과
 우선순위 큐 사용
 '''
+'''
 import sys
 input = sys.stdin.readline
 from queue import PriorityQueue
@@ -11,7 +12,7 @@ n = int(input())
 
 pq = PriorityQueue()
 for _ in range(n):
-	pq.put(int(input()))
+    pq.put(int(input()))
 
 total = 0
 temp = 0
@@ -21,3 +22,26 @@ while pq.qsize() != 1:
     total += temp
     pq.put(temp)
 print(total)    
+'''
+
+
+'''
+heap 사용
+Python3 216ms
+'''
+import sys
+input = sys.stdin.readline
+import heapq
+
+n = int(input())
+hq = []
+for _ in range(n):
+    heapq.heappush(hq, int(input()))
+# hq.sort() 
+# print(hq) 
+total = 0
+while len(hq) > 1:
+    temp = heapq.heappop(hq) + heapq.heappop(hq)
+    heapq.heappush(hq, temp)
+    total += temp
+print(total)
