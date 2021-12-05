a7501e90d6a76442085eceecbc814661b2850707
Yijin <yijin3018@gmail.com>
2021-12-05 23:31:17 +0900

    update 강의실 배정(이진).py

--- Problem Solving/05주차/강의실 배정(이진).py
+++ Problem Solving/05주차/강의실 배정(이진).py
@@ -1,5 +1,22 @@
-import sys
+# 384ms
 import heapq
+import sys
 
 input = sys.stdin.readline
-n = int(input())
+ 
+N = int(input())
+arr = []
+for _ in range(N):
+    A, B = list(map(int, input().split()))
+    arr.append([A, B])
+ 
+arr.sort(key=lambda x: x[0])
+time = []
+heapq.heappush(time, arr[0][1])
+for i in range(1, N):
+	if time[0] > arr[i][0]:
+		heapq.heappush(time, arr[i][1])
+	else:
+		heapq.heappop(time)
+		heapq.heappush(time, arr[i][1])
+print(len(time))
