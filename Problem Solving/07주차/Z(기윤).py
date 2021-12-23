6f2be387aa57fdfceef52d6a41008640729c4c8a
Giyoon Park <workspaceforict@gmail.com>
2021-12-24 01:38:36 +0900

    Add: Z(giyoon)

---
+++ Problem Solving/07주차/Z(기윤).py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+n, r, c = map(int, input().split())
+answer = 0
+
+def solve(y, x, size):
+    global answer
+    if (y, x) == (r, c):
+        print(answer)
+        return
+
+    if y <= r < y + size and x <= c < x + size:
+        solve(y, x, size//2)
+        solve(y, x+size//2, size//2)
+        solve(y+size//2, x, size//2)
+        solve(y+size//2, x+size//2, size//2)
+    
+    else: answer += size**2
+
+
+if __name__ == "__main__":
+    solve(0, 0, 2**n)
