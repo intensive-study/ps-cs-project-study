8d30daab2da8acfb9de8b51342a90005ba9652e0
Giyoon Park <workspaceforict@gmail.com>
2021-12-28 18:49:17 +0900

    update

--- Problem Solving/08주차/수들의 합(기윤).py
+++ Problem Solving/08주차/수들의 합(기윤).py
@@ -1,25 +1,17 @@
 import sys
 
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-arr = list(map(int, input().split()))
-left = 0
-right = 0
+n, m = map(int, sys.stdin.readline().rsplit())
+arr = list(map(int, sys.stdin.readline().rsplit()))
+sp, ep = 0, 0
 answer = 0
 
-while left < n:
-    total = sum(arr[left:right+1])
+while ep < n:
+    total = sum(arr[sp:ep+1])
     if total == m:
         answer += 1
-        left = right
-        right = left + 1
-    
-    elif total > m: left += 1
-    else: right += 1
-
-    if right == n:
-        left += 1
-        right = left
+        ep += 1
+    elif sp == ep: ep += 1
+    elif total > m: sp += 1
+    else: ep += 1
 
 print(answer)
