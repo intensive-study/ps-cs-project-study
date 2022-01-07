8a502ca347cb39c9f2af59b37a63190415aa0a43
Yijin <yijin3018@gmail.com>
2022-01-07 10:52:36 +0900

    Add: 반도체 설계(이진)

---
+++ Problem Solving/09주차/반도체 설계(이진).py
@@ -0,0 +1,20 @@
+def sol(nums,N):
+    dp = [1] * N
+    for i in range(1,N):
+        for j in range(i):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j]+1)
+    return max(dp)
+
+
+N = int(input())
+nums = list(map(int,sys.stdin.readline().split()))
+print(sol(nums,N))
+
+4 2 6 3 1 5  dp= [1,1,1,1,1,1]
+  2          dp= [1,1,1,1,1,1]
+	6        dp= [1,1,2,1,1,1]
+	  3      dp= [1,1,2,2,1,1]
+	    1    dp= [1,1,2,2,1,1]
+	      5  dp= [1,1,2,2,1,2]
+	         dp= [1,1,2,2,1,3]
